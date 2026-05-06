e0bc392b3d8070930ce947d1a88018d6670d2958
Paweł Sułkowski <sulkowski.pawel@gmail.com>
2026-05-06 13:32:30 +0200

    force-recreate when deploying the app

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 git pull origin main
 npm ci && npm run build:css
-docker compose -f docker-compose.prod.yml up -d --build
+docker compose -f docker-compose.prod.yml up -d --build --force-recreate
 docker image prune -f
